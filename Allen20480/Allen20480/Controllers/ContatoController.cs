0eff76a370e62de4f6c01de4a158c61d01336467
glautter <glautterg@gmail.com>
2015-08-20 15:28:15 -0700

    Requisição ajax
    Chamada de ajax

---
+++ Allen20480/Allen20480/Controllers/ContatoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Allen20480.Controllers
+{
+    public class ContatoController : Controller
+    {
+        //
+        // GET: /Contato/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Utilizo este attribute "HttpPost" para não passar o nome, email pela URL
+        [HttpPost]
+        public ActionResult EnviaMensagem(string nome, string email, string mensagem)
+        {
+            //Esperar 1 segundo
+            Thread.Sleep(1000);
+
+            //Retorna uma mensagem de "OK"
+            return Json(new { Message = "OK" });
+        }
+
+    }
+}
