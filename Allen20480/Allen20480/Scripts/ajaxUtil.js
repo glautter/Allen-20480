2b59ea0bab4c3c4f9dac9eee5996e627037cb35f
glautter <glautterg@gmail.com>
2015-08-24 16:51:51 -0700

    map
    localização

---
+++ Allen20480/Allen20480/Scripts/ajaxUtil.js
@@ -0,0 +1,17 @@
+﻿var Ajax = function (options) {
+    var _url = options.url;
+    var _type = options.type;
+    var _dataType = options.dataType;
+    var _data = options.data;
+    var _contentType = options.contentType;
+    
+    this.makeRequest = function () {
+        return $.ajax({
+            url: _url,
+            type: _type,
+            dataType: _dataType,
+            contentType: _contentType,
+            data: _data
+        }).promise();
+    }
+}
